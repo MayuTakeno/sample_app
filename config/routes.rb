f6704eee88577cb3f141d3e355b7116849cfe119
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-08-07 06:32:44 +0000

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'comment/index'
-  get 'comment/new'
-  get 'comment/show'
-  get 'comment/edit'
+
+  resources :books, only: [:index, :show] do
+    resources :comment, only: [:index, :new, :show, :edit]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
