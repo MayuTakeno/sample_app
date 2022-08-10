7943990df3e3f88ee3241e74d819764d5ec4376d
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-08-10 08:12:42 +0000

    ビュー追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+
     if @book.save
     else
       render :index
@@ -20,6 +21,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body )
   end
 end
