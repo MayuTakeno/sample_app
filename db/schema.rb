e18167fe6adb1bb22813c7ac69f2647418523609
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-08-22 12:16:54 +0000

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_05_120852) do
+ActiveRecord::Schema.define(version: 2022_08_21_115340) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +51,13 @@ ActiveRecord::Schema.define(version: 2022_08_05_120852) do
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower"
+    t.integer "following"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
