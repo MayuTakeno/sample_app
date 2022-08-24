9a24ff663b050fff554b9c6cf800fed6f03b731b
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-08-24 08:07:30 +0000

    アソシエーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,5 @@ class User < ApplicationRecord
         has_many :relationships, foreign_key: :following_id
         has_many :followings, through: :relationships, source: :follower
         has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
-        has_many :followers
+        has_many :followers, through: :reverse_of_relationships, source: :following
 end
